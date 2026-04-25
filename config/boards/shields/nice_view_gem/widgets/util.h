202a1d0897da65e9c3f3f769da08535286625542
essenceotd <github@jbmail.me>
2026-04-25 13:24:35 -0500

    Update util.h

--- config/boards/shields/nice_view_gem/widgets/util.h
+++ config/boards/shields/nice_view_gem/widgets/util.h
@@ -36,3 +36,4 @@ void init_rect_dsc(lv_draw_rect_dsc_t *rect_dsc, lv_color_t bg_color);
 void init_line_dsc(lv_draw_line_dsc_t *line_dsc, lv_color_t color, uint8_t width);
 void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
                     lv_text_align_t align);
+void draw_image(lv_obj_t *canvas, int x, int y, const lv_image_dsc_t *img_dsc, lv_draw_image_dsc_t *draw_dsc);
