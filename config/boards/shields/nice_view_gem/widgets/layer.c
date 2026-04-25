42f2418bb53c94682d0b9cf8e9f712e88f5a4758
essenceotd <github@jbmail.me>
2026-04-25 13:58:46 -0500

    Update layer.c

--- config/boards/shields/nice_view_gem/widgets/layer.c
+++ config/boards/shields/nice_view_gem/widgets/layer.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #include "layer.h"
+#include "util.h"
 #include "../assets/custom_fonts.h"
 #include <zmk/physical_layouts.h>
 #include <zmk/keymap.h>
@@ -25,5 +26,5 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
         layer_name = fallback_layer_name;
     }
 
-    lv_canvas_draw_text(canvas, 0, 70, SCREEN_WIDTH, &label_dsc, layer_name);
+    draw_text(canvas, 0, 70, SCREEN_WIDTH, &label_dsc, layer_name);
 }
